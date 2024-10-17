04ddcddf09f5d5c783d7734e5988597704e3a84f
beh0907 <qkrdlsguq@naver.com>
2024-10-17 16:18:05 +0900

    version 1.33
    - DataStore hilt 초기화 코드 미반영 코드 업데이트

--- app/src/main/java/com/skymilk/chatapp/di/DataStoreModule.kt
+++ app/src/main/java/com/skymilk/chatapp/di/DataStoreModule.kt
@@ -1,25 +1,39 @@
 package com.skymilk.chatapp.di
 
 import android.content.Context
+import android.preference.PreferenceDataStore
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.skymilk.chatapp.store.data.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
-    //dataStore 설정
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = Constants.SETTING_DATA_STORE)
 
-    //dataStore 초기화
     @Singleton
     @Provides
-    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> = context.dataStore
+    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler {
+                it.printStackTrace()
+                emptyPreferences()
+            },
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile = { context.preferencesDataStoreFile(Constants.SETTING_DATA_STORE) }
+        )
+    }
 }
